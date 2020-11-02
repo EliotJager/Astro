d26ac090498faddf40ac986fc69adb94b234b2fd
eliot.jager <eliot.jager@gmail.com>
2020-11-02 17:10:55 +0100

    new append_taub

--- postpro/bash/append_taub.sh
+++ postpro/bash/append_taub.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
 
-sce=/mnt/data/ejager/Greenland
+sce=/mnt/data/ejager/Greenland/
 
-mkdir /mnt/data/ejager/Greenland/data/TAUB
+mkdir $sce/RUNS2/TAUB
 
-output=/mnt/data/ejager/Greenland/data/TAUB
+output=$sce/RUNS2/TAUB
 
-files1=$(ls $sce/data/LatinHyper40/ | grep 'PARAM')
+files1=$(ls $sce/RUNS2/LH40/ | grep 'PARAM')
 
 echo $files
-
-c=0
-
-cur_param=$(head -n 3 $sce/data/LatinHyper40/test_PARAM0/paramater.txt)
-echo $cur_param
-m="${cur_param[@]:(-1)}"
-echo "m:"$m
-sed "s|<m>|"$m"|g;s|<INPUT>|"$sce"/data/LatinHyper40/test_PARAM0/MESH_2|g;s|<OUTPUT>|/mnt/data/ejager/Greenland/data/TAUB|g;s|<NUM>|0|g" $sce/postpro/pvpython/taub.py > $output/taub0.py
-pvpython $output/taub0.py
-rm $output/taub0.py
-c=$(($c+1)) # pour generaliser : supprimer les 2 lignes
-
 
 echo $files1
 for f in $files1
@@ -29,30 +17,23 @@ do
     echo $f
     num=${f[@]:5:6}
     echo 'num: '$num
-    cur_param=$(head -n 3 $sce/data/LatinHyper40/$f/paramater.txt)
-    echo $cur_param
-    m="${cur_param[@]:(-1)}"
-    echo "m:"$m
-    sed "s|<m>|"$m"|g;s|<INPUT>|"$sce"/data/LatinHyper40/$f/MESH_2|g;s|<OUTPUT>|/mnt/data/ejager/Greenland/data/TAUB|g;s|<NUM>|$num|g" $sce/postpro/pvpython/taub.py > $output/taub$num.py
+    size=${#num}
+    echo 'size num: '$size
+    if [ $size -eq 1 ]
+    then
+        echo '1'
+        cur_param=$(head -n 3 $sce/RUNS2/LH40/$f/parameter.txt)
+        echo $cur_param
+        m="${cur_param[@]:85:4}"
+        echo "m:"$m
+    else
+        echo '2'
+        cur_param=$(head -n 3 $sce/RUNS2/LH40/$f/parameter.txt)
+        echo $cur_param
+        m="${cur_param[@]:88:4}"
+        echo "m:"$m    
+    fi
+    sed "s|<m>|"$m"|g;s|<INPUT>|"$sce"RUNS2/LH40/$f/MESH_2|g;s|<OUTPUT>|$output|g;s|<NUM>|$num|g" $sce/postpro/pvpython/taub.py > $output/taub$num.py
     pvpython $output/taub$num.py
     rm $output/taub$num.py
-    c=$(($c+1))
-done
-
-
-files2=$(ls $sce/data/LatinHyper40/m1_fine/ | grep 'PARAM')
-
-for f in $files2
-do
-    echo $f
-    num=${f[@]:5:6}
-    echo 'num: '$num
-    cur_param=$(head -n 3 $sce/data/LatinHyper40/m1_fine/$f/paramater.txt)
-    echo $cur_param
-    m="${cur_param[@]:(-1)}"
-    echo "m:"$m
-    sed "s|<m>|"$m"|g;s|<INPUT>|"$sce"/data/LatinHyper40/m1_fine/$f/MESH_2|g;s|<OUTPUT>|/mnt/data/ejager/Greenland/data/TAUB|g;s|<NUM>|$num|g" $sce/postpro/pvpython/taub.py > $output/taub$num.py
-    pvpython $output/taub$num.py
-    rm $output/taub$num.py
-    c=$(($c+1))
 done
